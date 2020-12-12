ef0b56e7a675173ce4e49fa476fd60bc5f8316d4
Ilya Jukov <jukov46@yandex.ru>
2020-12-12 18:28:28 +0300

    Add new language (chinies)

---
+++ Courses/Courses/Languages/China.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Courses.Courses
+{
+    class China : Course
+    {
+        public China(string courseName, string teacherName, string language, double price)
+        {
+            CourseName = courseName;
+            TeacherName = teacherName;
+            Language = language;
+            Price = price;
+        }
+    }
+}
